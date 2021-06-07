ab709639accd25978fb2fda0a264f1e33aa40410
Savo <savo.kos.sk@gmail.com>
2021-06-07 17:33:49 +0200

    Removed env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   target: 'serverless',
-  assetPrefix: isProd ? 'https://media.rawg.io' : '',
   images: {
     domains: ['media.rawg.io'],
   },
