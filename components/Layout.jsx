06191e1dcc21d07e65adcbe674336adb770967ba
Savo <savo.kos.sk@gmail.com>
2021-07-04 13:32:34 +0200

    Dynamic page title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,7 +13,7 @@ export default function Layout({
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <title>Game Portal</title>
+        <title>{title}</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} />
